13063d7a510f85565844be7b5f5216536f046d20
kimlong0 <kimlong0@uw.edu>
2023-02-24 23:47:10 -0800

    logo test

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,8 @@
 function Navbar() {
   return (
-    <div>navbar</div>
+    <div className="md:px-10 px-8 py-6 bg-slate-400">
+        <p className="text-2xl font-semibold">CDP Vault Liquidation Calculator</p>
+    </div>
   )
 }
 
